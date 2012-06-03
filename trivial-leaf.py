a1b3eda6bd0f7832c7217de5b7aa4393f3565fde
Aaron Schumacher <ajschumacher@gmail.com>
2012-06-03 12:28:28 -0400

    state as of last night

--- trivial-leaf.py
+++ trivial-leaf.py
@@ -8,6 +8,12 @@ from google.appengine.ext.webapp import template
 
 class RootResponse(webapp2.RequestHandler):
     def get(self):
+        data_urls = self.request.get_all('d')
+        try:
+            urlfetch.fetch(url = data_urls[0],
+                           method = urlfetch.POST)
+        except:
+            pass
         template_values = {}
         path = os.path.join(os.path.dirname(__file__), 'leaf.html')
         self.response.out.write(template.render(path, template_values))
